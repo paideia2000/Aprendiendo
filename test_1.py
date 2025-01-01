dc6327e342cc6e636fd87cc9533e217470db92e1
Rene Medina <poencima22@gmail.com>
2025-01-01 11:50:42 +0100

    Juego de la adivinanza

--- test_1.py
+++ test_1.py
@@ -1,26 +1,39 @@
 from random import randint
 
-numeros = randint(0,10)#generar numeros aleatorios
+
 
 def choise_user():#identificar el numero ganador o continuar bucle sino
     while True:
-        try:
-            choise = int(input("Ingrese el numero que usted cree ganador en el rango del (1-10): "))
-            if choise > numeros:
-                if choise > 10:
-                    print("Error: por favor inserte un numero menor que (10)")
-                    continue
-                print("Uff, se ha pasado un poco, vuelva a intentar reduciendo el numero")
-            elif choise < numeros:
-                if choise <= 0:
-                    print("Error: por favor inserte un numero mayor que (1)")
-                    continue
-                print("Uff, se ha quedado corto, vuelva a intentar aumentando  el numero")
-            else:
-                print("Has aceptado tu ganas")
-                break
-        except ValueError:
-            print("Error: Inserte numeros por favor")
+        numero = randint(1,10)#generar numero aleatorios
+        while True:
+            try:
+                choise = int(input("Ingrese el numero que usted cree ganador en el rango del (1-10): "))
+                if choise < 1 or choise >  10:
+                    print("Error: Inserte numero en el rango del 1-10")
+                elif choise > numero:
+                    print("Uff, se ha pasado un poco, vuelva a intentar reduciendo el numero")
+                elif choise < numero:
+                    print("Uff, se ha quedado corto, vuelva a intentar aumentando  el numero")
+                else:
+                    print("Has aceptado tu ganas")
+                    break
+            except ValueError:
+                print("Error: Inserte numero por favor")
+    
+        cerrar = input("Desea cerrar el juego? (SI/NO): ").lower().strip()
+        if cerrar == "si":
+            print("¡Hasta la proxima!")
+            break
+        elif cerrar == "no":
+            print("\n¡Nuevo juego iniciado!")
+        else:
+            print("Incinuamos que quizo cerrar el programa. ¡Hasta la proxima!")
+            break
+            
+
 
 choise_user()
+
+
+
 
