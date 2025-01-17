686d4955ef67698eee481f910eb53354e67ec25f
Rene Medina <poencima22@gmail.com>
2025-01-17 16:03:46 +0100

    Cambios en la calculadora

--- SAMPLES/calculadora.py
+++ SAMPLES/calculadora.py
@@ -8,34 +8,34 @@ def interfaz():
     
 #ejecutar el programa
 def ejecutar():
+    operaciones = ["+", "-", "*", "/"]
     while True:
+        operador = input("\n""Cual operaciòn desea realizar -> (+ , - , * , / ): ").strip().lower()#indicar que operador usar
+        if operador not in operaciones:
+            print("¡Error!. Por favor inserte un operador vàlido")
+            continue
+
         number_1 = float(input("Ingrese el primer numero.: "))
-        operador = input( "-> (+ , - , * , / ):  ").strip()#indicar que operador usar
         number_2 = float(input("Ingrese el segundo numero.: "))
         
         if operador == "+":
             suma = lambda num1, numb2: num1 + numb2
-            print("\n" + "El resultado de la suma es: {}".format(suma(number_1, number_2)))
-        
+            print("El resultado de sumar {} + {} = {}.".format(number_1, number_2, suma(number_1, number_2)))
+
         elif operador == "-":
             resta = lambda num1, num2: num1 - num2
-            print("El resultado de la resta es: {}".format(resta(number_1, number_2)))
-        
+            print("El resultado de restar {} - {} = {}.".format(number_1, number_2, resta(number_1, number_2)))
+
         elif operador == "*":
             multi = lambda num1, num2: num1 * num2
-            print("El resultado de la multiplicacion es: {}".format(multi(number_1, number_2)))
-        
-        elif operador == "/":
-            divi = lambda num1, num2: num1 / num2
-            print("El resultado de la divicion  es: {}".format(divi(number_1, number_2)))
-        
-        elif operador == "%":
-            multi = lambda num1, num2: num1 * num2
-            print("El resultado de la multiplicacion es: {}".format(multi(number_1, number_2)))
-            
-        else:
-            print("\n" + "¡Operador invàlido!, indique alguno de los cuales se les muestra")
+            print("El resultado de multiplicar {} * {} = {}.".format(number_1, number_2, multi(number_1, number_2)))
 
-        
+        else:
+            if number_2 == 0:
+                print("¡Error!. No se puede dividir por cero(0).")
+            else:
+                divi = lambda num1, num2: num1 / num2
+                print("El resultado de dividir {} / {} = {}.".format(number_1, number_2, divi(number_1, number_2)))
+            
 ejecutar()
 
