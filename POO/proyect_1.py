1efb8d3ae75047582e1c2e9ac9093b199db6a75d
Renecito Medina <poencima22@gmail.com>
2025-01-22 10:22:18 +0100

    Proyecto nuevo, cambios en proyecto1

--- POO/proyect_1.py
+++ POO/proyect_1.py
@@ -1,6 +1,5 @@
 #liga de futboll con programas: clases(Equipos,Partidos,Liga)
 import random
-
 class Equipo:
     def __init__(self, nombre):
         self.nombre = nombre
@@ -15,8 +14,7 @@ class Equipo:
             self.puntos += 3
         elif goles_a_favor == goles_en_contra:
             self.puntos += 1
-        
-#Partidos
+
 class Partido:
     def __init__(self, equipo1, equipo2):
         self.equipo1 = equipo1
@@ -29,7 +27,6 @@ class Partido:
         self.equipo2.update_puntos(goles_equipo2, goles_equipo1)
         print(f"{self.equipo1.nombre} {goles_equipo1} - {goles_equipo2} {self.equipo2.nombre}")
 
-#Clase liga
 class Liga:
     def __init__(self, equipos):
         self.equipos = equipos
@@ -39,17 +36,13 @@ class Liga:
         print("Tabla de Posiciones\n".center(100))
         for equipo in self.equipos:
             print(f"{equipo.nombre}: {equipo.puntos} puntos -- Goles_a_Favor {equipo.goles_a_favor}, -- Goles_en_Contra {equipo.goles_en_contra}")
-    
+
     def enfrentamiento(self):
         print("Partidos Jugados\n".center(100))
         for i in range(len(self.equipos)):
             for j in range(i+1, len(self.equipos)):
                 partidos = Partido(self.equipos[i], equipos[j])
                 partidos.jugar()
-        
-    
-
-
 
 python = Equipo("Python")
 java = Equipo("Java")
