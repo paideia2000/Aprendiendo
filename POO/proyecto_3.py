9b3b9b4d22f50fe828fcc149804433c9746706f8
paideia2000 <poencima22@gmail.com>
2025-01-30 13:32:31 +0100

    Arreglos en proyecto_3

--- POO/proyecto_3.py
+++ POO/proyecto_3.py
@@ -1,35 +1,38 @@
 #biblioteca
-from abc import ABC
-
-class Persona(ABC):
+from abc import ABC, abstractmethod
+        
+class Usuario(ABC):
     def __init__(self, nombre, email):
         self.nombre = nombre
         self.email = email
-
-    def mostrar_atributos(self):
-        print(f"Su nombre de usuario es: {self.nombre} y su email es: {self.email}")
-
-class Usuario(Persona):
-    def __init__(self, nombre, email):
-        super().__init__(nombre, email)
         self.libros_reservados = []
         self.libros_devueltos = []
+    
+    @abstractmethod
+    def mostrar_atributos(self):
+        pass
 
 class Adquirir_prestado(Usuario):
     def __init__(self,nombre, email):
         super().__init__(nombre, email)
-
+    
+    def mostrar_atributos(self):
+        print(f"Su nombre de usuario es: {self.nombre} y su email es: {self.email}")
+        
     def adquirir_prestado(self, libro, prestar):
         if prestar.prestar_libro(libro):
             self.libros_reservados.append(libro)
-            print(F"El libro:'{libro}', ha sido prestado ha:'{self.nombre}', su email es:'{self.email}'")
+            print(F"El libro:'{libro}', ha sido prestado ha:'{self.nombre}'. Su email es:'{self.email}'")
         else:
             print(f"El libro: {libro} no lo tenemos en la estanteria, lo sentimos.")
 
-class Devolver_libro():
+class Devolver_libro(Usuario):
     def __init__(self,nombre, email):
         super().__init__(nombre, email)
-
+        
+    def mostrar_atributos(self):
+        print(f"Su nombre de usuario es: {self.nombre} y su email es: {self.email}")
+    
     def devolver_libro(self, libro):
         if libro in self.libros_reservados:
             self.libros_reservados.remove(libro), self.libros_devueltos.append(libro)
@@ -55,7 +58,17 @@ class Recuperar_libro(Biblioteca):
         if libro not in self.estanteria:
             self.estanteria.append(libro)
             return True
-            
 
+
+#intancias de las ABC Biblioteca
 prestar = Prestar_libro()
-recuperar = Devolver_libro()
+recuperar = Recuperar_libro()
+
+#intancias de las ABC Usuario
+usuario1 = Adquirir_prestado("Rene", "poe22@gmail.com")
+usuario1.mostrar_atributos()
+usuario1.adquirir_prestado("Padre Rico", prestar)
+
+usuario2 = Adquirir_prestado("Ruben", "rueb@gmail.com")
+usuario2.mostrar_atributos()
+usuario2.adquirir_prestado("Pae Rico", prestar)
